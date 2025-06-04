f30a2ac3add0b9bc2cfd4d114f122fa92cd95f0e
carlasolrocca <carlasolrocca@gmail.com>
2025-06-04 16:34:40 -0300

    Atributos en Persona y notas en Administrador

--- src/main/kotlin/Administrador.kt
+++ src/main/kotlin/Administrador.kt
@@ -1,9 +1,12 @@
 /*
 * 1. el admin deberia buscar el primer regalo compatible con su preferencia en base al STOCK
   2. si ningun regalo es apto, se le genera COMO REGALO un voucher de $2000 (marca="Papapp")
-
-
-
+  3. se debe registrar el regalo ENTREGADO a la persona. ¿Como? Eso dispara una serie de OBSERVERS
+            3a. que mande mail a la persona. "tu regalo fue entregado!"
+            3b. informa a empresa fletes "el reno loco": recibe el regalo, la direccion del cliente,
+            nombre del cliente, DNI, codigo del regalo (c/u tiene el suyo)
+            3c. si algun regalo supera los $10000, se pide que modifique el criterio de la persona interesada
+            por un monto de $5000. NO TIENE SENTIDO!!!! NO ES LO QUE HACE ESE STRATEGY. No se si resolverlo...
 */
 
 class Administrador {
--- src/main/kotlin/Persona.kt
+++ src/main/kotlin/Persona.kt
@@ -2,6 +2,7 @@ class Persona(
     val nombre : String,
     val DNI : Int = 12345678,
     val email : String = "persona@gmail.com",
+    val direccion : String = "Calle Falsa 123",
     val preferenciaRegalos : PreferenciaPorRegalos,
 ){
 
