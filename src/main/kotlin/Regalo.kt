ffb6481abd14526ee22808fd8892022e6bba3334
carlasolrocca <carlasolrocca@gmail.com>
2025-06-04 15:51:47 -0300

    Cambio Regalo

--- src/main/kotlin/Regalo.kt
+++ src/main/kotlin/Regalo.kt
@@ -8,35 +8,41 @@ abstract class Regalo(
     val codigo : Int = 1234
 
     //Template Method: determina si un regalo es valioso o no.
-    //Lo es cuando precio>5000.0 y se cumple la condicion particular propia de c/Regalo
     fun esValioso(): Boolean {
-        return precio > 5000.0 &&  criterioEsValioso()
+        return condicionGeneralValioso() && criterioEsValioso()
     }
+
+    //Condicion general y particular. Se usan en el Template Method
+    fun condicionGeneralValioso() : Boolean = precio > 5000.0
     abstract fun criterioEsValioso() : Boolean
 }
 
 // *** Subclases de Regalo ***
+
+//Si la marca está en la lista, es valioso
 class Ropa(marca : String, precio : Double) : Regalo(marca, precio) {
     override fun criterioEsValioso(): Boolean {
         val listaMarcasValiosas = mutableListOf("Jordache", "Lee", "Charro", "Motor Oil")
-        return listaMarcasValiosas.contains(marca)  //Si la marca está en la lista, es valioso
+        return listaMarcasValiosas.contains(marca)
     }
 }
 
+//Si se lanzaron antes del año 2000
 class Juguete(marca : String, precio : Double, val fechaLanzamiento : LocalDate) : Regalo(marca, precio) {
-    //Si se lanzaron antes del año 2000
     override fun criterioEsValioso(): Boolean = fechaLanzamiento.isBefore(LocalDate.of(2000, 1, 1))
 }
 
+//Si es de origen extranjero
 class Perfume(marca : String, precio : Double, val origen : OrigenRegalo) : Regalo(marca, precio) {
-    //Si es de origen extranjero
+
     override fun criterioEsValioso(): Boolean = origen == OrigenRegalo.EXTRANJERO
 }
 
+//Si la experiencia es un viernes
 class Experiencia(marca : String, precio : Double, val diaExperiencia : LocalDate) : Regalo(marca, precio) {
-    //Si la experiencia es un viernes
     override fun criterioEsValioso(): Boolean = diaExperiencia.dayOfWeek == DayOfWeek.FRIDAY
 }
+
 
 enum class OrigenRegalo {
     NACIONAL,
