dc05263445fefc282e5a57bbcb3946780d401d2e
carlasolrocca <carlasolrocca@gmail.com>
2025-06-04 15:39:24 -0300

    criterioEsValioso() de todas las subclases de Regalo

--- src/main/kotlin/Regalo.kt
+++ src/main/kotlin/Regalo.kt
@@ -1,3 +1,6 @@
+import java.time.DayOfWeek
+import java.time.LocalDate
+
 abstract class Regalo(
     val marca : String = "",
     val precio : Double = 0.0,
@@ -5,7 +8,7 @@ abstract class Regalo(
     val codigo : Int = 1234
 
     //Template Method: determina si un regalo es valioso o no.
-    //Cuando es precio>5000.0 y cuando se cumple la condicion particular propia de c/Regalo
+    //Lo es cuando precio>5000.0 y se cumple la condicion particular propia de c/Regalo
     fun esValioso(): Boolean {
         return precio > 5000.0 &&  criterioEsValioso()
     }
@@ -15,25 +18,27 @@ abstract class Regalo(
 // *** Subclases de Regalo ***
 class Ropa(marca : String, precio : Double) : Regalo(marca, precio) {
     override fun criterioEsValioso(): Boolean {
-        TODO("Not yet implemented")
+        val listaMarcasValiosas = mutableListOf("Jordache", "Lee", "Charro", "Motor Oil")
+        return listaMarcasValiosas.contains(marca)  //Si la marca está en la lista, es valioso
     }
 }
 
-class Juguete(marca : String, precio : Double) : Regalo(marca, precio) {
-    override fun criterioEsValioso(): Boolean {
-        TODO("Not yet implemented")
-    }
+class Juguete(marca : String, precio : Double, val fechaLanzamiento : LocalDate) : Regalo(marca, precio) {
+    //Si se lanzaron antes del año 2000
+    override fun criterioEsValioso(): Boolean = fechaLanzamiento.isBefore(LocalDate.of(2000, 1, 1))
 }
 
-class Perfume(marca : String, precio : Double) : Regalo(marca, precio) {
-    override fun criterioEsValioso(): Boolean {
-        TODO("Not yet implemented")
-    }
+class Perfume(marca : String, precio : Double, val origen : OrigenRegalo) : Regalo(marca, precio) {
+    //Si es de origen extranjero
+    override fun criterioEsValioso(): Boolean = origen == OrigenRegalo.EXTRANJERO
 }
 
-class Experiencia(marca : String, precio : Double) : Regalo(marca, precio) {
-    override fun criterioEsValioso(): Boolean {
-        TODO("Not yet implemented")
-    }
+class Experiencia(marca : String, precio : Double, val diaExperiencia : LocalDate) : Regalo(marca, precio) {
+    //Si la experiencia es un viernes
+    override fun criterioEsValioso(): Boolean = diaExperiencia.dayOfWeek == DayOfWeek.FRIDAY
 }
 
+enum class OrigenRegalo {
+    NACIONAL,
+    EXTRANJERO
+}
